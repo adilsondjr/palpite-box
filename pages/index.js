33b64715e3b998c4c4cd5b3419266a4a33d7ca21
Adilson Jr <adilsondjr@gmail.com>
2020-11-04 18:46:53 -0300

    Getting data from sheet with useSWR and API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Index = () => {
+  const { data, error } = useSWR('/api/get-promo', fetcher)
+
   return (
     <div>
       <p className='mt-6 text-center'>
@@ -15,9 +20,12 @@ const Index = () => {
         </Link>
       </div>
 
-      <p className='my-12 text-center'>
-        Mensagem do desconto
-      </p>
+      { !data && <p>Carregando...</p> }
+      { !error && data && data.showCoupon &&
+        <p className='my-12 text-center'>
+          {data.message}
+        </p>
+      }
     </div>
   )
 }
